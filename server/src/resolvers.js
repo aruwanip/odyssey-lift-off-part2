3f7cc77e2bb74d57708a270c1bf90cbe95c14d3d
Pinkie Aruwani <aruwanip@gmail.com>
2022-09-26 19:26:36 -0400

    Implementing resolvers

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,7 +1,14 @@
 const resolvers = {
     Query: {
         // returns an array of Tracks that will be used to populate the homepage grid of our web client
-        tracksForHome: (parent, args, context, info) => {}
+        tracksForHome: (_, __, { dataSources }) => {
+            return dataSources.trackAPI.getTraksForHome();
+        }
+    },
+    Track: {
+        author: ({ authorId }, _, { dataSources }) => {
+            return dataSources.trackAPI.getAuthor(authorId);
+        }
     }
 };
 
